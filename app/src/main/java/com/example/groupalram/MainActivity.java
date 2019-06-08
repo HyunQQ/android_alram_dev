977902838d3bd1416462e37b6701e3a32068d286
2mh0829 <emh0829@gmail.com>
2019-06-08 17:54:11 +0900

    main design

--- app/src/main/java/com/example/groupalram/MainActivity.java
+++ app/src/main/java/com/example/groupalram/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.groupalram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnAddAlranClick(View v){
+        Log.d("btn", "알람 추가창으로 이동");
+        //Intent intent = new Intent(this, AlramSetActivity.class);
+        //startActivity(intent);
     }
 }
