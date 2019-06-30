63aa2f9196c16cbb1bf4775cbfd48bd9d29a8a3a
hyunq <qqkqqk23@naver.com>
2019-06-30 21:49:23 +0900

    AlramSetActivity slec group, option 팝업 메뉴 테스트 추가

--- app/src/main/java/com/example/groupalram/AlramSetActivity.java
+++ app/src/main/java/com/example/groupalram/AlramSetActivity.java
@@ -13,22 +13,27 @@ public class AlramSetActivity extends AppCompatActivity implements View.OnClickL
     private Button btnSetAlramSave;
     private Button btnSelcBell;
     private Button btnSelcCycle;
-    
+    private Button btnSelcGroup;
+    private Button btnSelcOpt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alram_set);
 
-        btnSetAlramCancle = (Button) findViewById(R.id.btn_set_alram_cancle);
-        btnSetAlramSave = (Button) findViewById(R.id.btn_set_alram_save);
-        btnSelcBell = (Button) findViewById(R.id.btn_selc_bell);
-        btnSelcCycle = (Button)findViewById(R.id.btn_selc_cycle);
+        btnSetAlramCancle   = (Button) findViewById(R.id.btn_set_alram_cancle);
+        btnSetAlramSave     = (Button) findViewById(R.id.btn_set_alram_save);
+        btnSelcBell         = (Button) findViewById(R.id.btn_selc_bell);
+        btnSelcCycle        = (Button)findViewById(R.id.btn_selc_cycle);
+        btnSelcGroup        = (Button)findViewById(R.id.btn_selc_group);
+        btnSelcOpt          = (Button)findViewById(R.id.btn_selc_opt);
 
         btnSetAlramCancle.setOnClickListener(this);
         btnSetAlramSave.setOnClickListener(this);
         btnSelcBell.setOnClickListener(this);
         btnSelcCycle.setOnClickListener(this);
+        btnSelcGroup.setOnClickListener(this);
+        btnSelcOpt.setOnClickListener(this);
 
     }
     @Override
@@ -53,7 +58,7 @@ public class AlramSetActivity extends AppCompatActivity implements View.OnClickL
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         switch (item.getItemId()){
-                            case R.id.m1:
+                            case R.id.alram_bell_1:
                                 Toast.makeText(getApplication(),"벨소리 리스트",Toast.LENGTH_SHORT).show();
                                 break;
                         }
@@ -88,6 +93,44 @@ public class AlramSetActivity extends AppCompatActivity implements View.OnClickL
                     }
                 });
                 pSelcCycle.show();
+                break;
+
+            case R.id.btn_selc_group:
+
+                PopupMenu pSelcGroup = new PopupMenu(this, v);
+                pSelcGroup.getMenuInflater().inflate(R.menu.selc_cycle_menu, pSelcGroup.getMenu());
+                pSelcGroup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.alram_group_1:
+                                Toast.makeText(getApplication(),"alram_group_1",Toast.LENGTH_SHORT).show();
+                                break;
+
+                        }
+                        return false;
+                    }
+                });
+                pSelcGroup.show();
+                break;
+
+            case R.id.btn_selc_opt:
+
+                PopupMenu pSelcOpt = new PopupMenu(this, v);
+                pSelcOpt.getMenuInflater().inflate(R.menu.selc_cycle_menu, pSelcOpt.getMenu());
+                pSelcOpt.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.alram_opt_1:
+                                Toast.makeText(getApplication(),"alram_opt_1",Toast.LENGTH_SHORT).show();
+                                break;
+
+                        }
+                        return false;
+                    }
+                });
+                pSelcOpt.show();
                 break;
         }
 
