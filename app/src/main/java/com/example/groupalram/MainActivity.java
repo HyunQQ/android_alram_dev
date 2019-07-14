ed960454abea42348067c69d0f45e1bb17132156
2mh0829 <emh0829@gmail.com>
2019-07-14 17:20:53 +0900

    동적 알람 생성

--- app/src/main/java/com/example/groupalram/MainActivity.java
+++ app/src/main/java/com/example/groupalram/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.groupalram;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.SQLException;
-import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DbOpenHelper mDbOpenHelper;
+    Context context;
+    LinearLayout layout;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +33,63 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        context = this;
+
+        layout = (LinearLayout) findViewById(R.id.layout_main);
 
         Button btnAddAlram = (Button) findViewById(R.id.btn_add_alram);
+        Button btnTemp = (Button) findViewById(R.id.btn_temp);
 
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnAddAlram.setOnClickListener(this);
+        btnTemp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_temp:
+
+
+                Button btn = new Button(context);
+                btn.setText("B_" + count++);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, AlramSetActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                //btn.setId();
+
+                Switch swc = new Switch(context);
+
+                LinearLayout alramRow = new LinearLayout(context);
+                alramRow.setOrientation(LinearLayout.HORIZONTAL);
+                alramRow.addView(btn);
+                alramRow.addView(swc);
+
+
+                LinearLayout groupRow = new LinearLayout(context);
+                groupRow.setOrientation(LinearLayout.VERTICAL);
+                groupRow.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.rounded));
+                groupRow.addView(alramRow);
+
+
+                layout.addView(groupRow);
+                break;
+
+            case R.id.btn_add_alram:
                 // 알람 추가
                 Intent intent = new Intent(MainActivity.this, AlramSetActivity.class);
                 startActivity(intent);
-            }
-        };
+                break;
+        }
 
-        btnAddAlram.setOnClickListener(listener);
     }
+
+
+
+
 
 }
