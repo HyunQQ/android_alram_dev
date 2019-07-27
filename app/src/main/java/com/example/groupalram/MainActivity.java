95f74b04901d31039ecb6b5d2ae46286afa3c8dd
2mh0829 <emh0829@gmail.com>
2019-07-27 19:06:40 +0900

    1. add group swtich control function 2. add choose group function

--- app/src/main/java/com/example/groupalram/MainActivity.java
+++ app/src/main/java/com/example/groupalram/MainActivity.java
@@ -6,9 +6,11 @@ import android.database.SQLException;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 
@@ -17,8 +19,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private DbOpenHelper mDbOpenHelper;
     Context context;
     LinearLayout layout;
-    int count = 0;
-    int groupCnt = 0;
+    int btnCnt = 0;
+    int swcIndex = 10000000;
+    int groupCnt = 100000000;
+    int groupSwcIndex = 100000000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +55,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()) {
             case R.id.btn_add_group:
-                groupCnt++;
+                final int groupId = ++groupCnt;
+                final int groupSwcId = groupCnt + groupSwcIndex;
 
-                Switch swc = new Switch(context);
+                final Switch swc = new Switch(context);
                 swc.setChecked(true);
 
                 LinearLayout.LayoutParams swcParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
                 swcParams.weight = 1;
                 swc.setLayoutParams(swcParams);
+                swc.setId(groupSwcId);
+                swc.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(swc.isChecked()){
+                            LinearLayout groupLayout = (LinearLayout) findViewById(groupId);
+                            for (int i=1; i<groupLayout.getChildCount(); i++){
+                                LinearLayout childLayout = (LinearLayout) groupLayout.getChildAt(i);
+                                Switch innerSwc = (Switch) findViewById(childLayout.getChildAt(1).getId());
+                                innerSwc.setChecked(true);
+                            }
+                        }else{
+                            LinearLayout groupLayout = (LinearLayout) findViewById(groupId);
+                            for (int i=1; i<groupLayout.getChildCount(); i++){
+                                LinearLayout childLayout = (LinearLayout) groupLayout.getChildAt(i);
+                                Switch innerSwc = (Switch) findViewById(childLayout.getChildAt(1).getId());
+                                innerSwc.setChecked(false);
+                            }
+                        }
+                    }
+                });
 
 
                 LinearLayout titleRow = new LinearLayout(context);
@@ -70,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 groupRow.setOrientation(LinearLayout.VERTICAL);
                 groupRow.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.rounded));
                 groupRow.addView(titleRow);
-                groupRow.setId(groupCnt);
+                groupRow.setId(groupId);
 
 
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
@@ -83,7 +109,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_temp:
 
-                int selectGroupId = groupCnt;
+                EditText groupNum = (EditText) findViewById(R.id.checked_group_num);
+                int selectGroupId = Integer.parseInt(groupNum.getText().toString());
                 LinearLayout selectedGroup = (LinearLayout) findViewById(selectGroupId);
 
                 // 알람 생성
@@ -99,30 +126,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void addAlramRow(LinearLayout selectedGroup){
+        final int btnId = ++btnCnt;
+        final int swcId = btnCnt + swcIndex;
+        final int groupId = selectedGroup.getId();
+
         Button btn = new Button(context);
+        btn.setBackgroundColor(0);
+        btn.setId(btnId);
+        btn.setText("7:45");
+        btn.setTextSize(40);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("[ID CHECK]", "btnId = " + btnId + " / groupId = " + groupId);
                 Intent intent = new Intent(MainActivity.this, AlramSetActivity.class);
                 startActivity(intent);
             }
         });
-        btn.setBackgroundColor(0);
-        //btn.setId("B_" + count++);
-        btn.setText("7:45");
-        btn.setTextSize(40);
+
 
         LinearLayout.LayoutParams btnParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
         btnParams.weight = 4;
         btn.setLayoutParams(btnParams);
 
 
-        Switch swc = new Switch(context);
+        final Switch swc = new Switch(context);
         swc.setChecked(true);
 
         LinearLayout.LayoutParams swcParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
         swcParams.weight = 1;
         swc.setLayoutParams(swcParams);
+        swc.setId(swcId);
+        swc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(swc.isChecked()){
+                    Log.d("[switch clike]", "[switch on] :" + swcId);
+                }else{
+                    Log.d("[switch clike]", "[switch off] :" + swcId);
+                }
+            }
+        });
 
 
         LinearLayout alramRow = new LinearLayout(context);
